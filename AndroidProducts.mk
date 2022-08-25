dfb803ced1f3fe09423a6a299b1d4b165e2848ef
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2022-08-25 11:11:16 +0300

    Initial skeleton device tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_a11q.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a11q-user \
+    lineage_a11q-userdebug \
+    lineage_a11q-eng
